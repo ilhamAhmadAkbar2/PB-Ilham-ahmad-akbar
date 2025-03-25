ee315a74d9907024cc644ebcb7422bfe53b222ec
ilhamAhmadAkbar2 <167837853+ilhamAhmadAkbar2@users.noreply.github.com>
2025-03-26 00:40:01 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+Sebelumnya kita sudah mempelajari bagaimana menghubungkan Aplikasi dengan Firebase, maka ada beberapa tugas yang mesti diselesaikan
+
+Tugas:
+1. Mensolve Code untuk SignUp agar dapat masuk ke Firebase
+2. Membuat halaman Home secara sederhana dengan menggunakan Cardview dan NestedScrollview
+3. Membuat halaman Profile yang menampilkan data user
+4. Jika sudah, pastikan Code di Push ke Github dengan nama Repository "PB-nama mahasiswa"
+
+Pada tugas sebelum nya, kalian sudah berhasil membuat beberapa tampilan dengan menggunakan Cardview dan NestedScrollview, maka ada beberapa tugas yang akan abang tambahkan sebelum kita UTS:
+
+1. Membuat Minimal 2-3 menu utama yang nanti akan ada pada tampilan Home (menu utama nya bebas, namun dapat menginputkan dan menampilkan data, contoh nya pencatatan keuangan ataupun memo)
+2. Buatlah tampilan yang menarik, kemudian buatlah tampilan NavigationBar (Disarankan terdapat 3 menu, Home, Profile, Pengaturan)
+3. Selamat mengerjakan
---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation (libs.activity.v190)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.database)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.gridlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.core:core-ktx:1.10.0")
+}
